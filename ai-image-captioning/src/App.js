c58a6011b32c5e8b5ef0c705934d5cdc4a1fce2a
SbonisoZama <sbonisozama535@gmail.com>
2025-03-02 17:25:28 +0200

    Created react app and intergrated ImageUpload component with App.js

---
+++ ai-image-captioning/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ImageUpload from './ImageUpload';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>AI Image Captioning</h1>
+       <ImageUpload />    
+      </header>
+    </div>
+  );
+}
+
+export default App;
